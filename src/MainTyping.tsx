660b0509470332fd3aac9a68d9d8ddf2d2ac2e5f
Felix <fhardjana@gmail.com>
2026-07-02 14:13:11 -0400

    testing commit

--- src/MainTyping.tsx
+++ src/MainTyping.tsx
@@ -5,6 +5,13 @@ const MainTyping = () => {
         <header>
             <img src="../assets/images/logo-large.svg" alt="Typing Speed Logo" />
         </header>
+
+        <div className="stats-bar">
+            <div>WPM:</div>
+            <div>Accuracy:</div>
+            <div>Time:</div>
+            <div>Difficulty</div>
+        </div>
         </>
     );
 }
