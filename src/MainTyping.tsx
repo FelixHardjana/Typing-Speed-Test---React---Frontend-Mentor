a5f6341f1a8d84b100b3acc2ba1cadb5b05fc1d0
Felix <fhardjana@gmail.com>
2026-07-02 13:51:37 -0400

    getting started

---
+++ src/MainTyping.tsx
@@ -0,0 +1,12 @@
+
+const MainTyping = () => {
+    return(
+        <>
+        <header>
+            <img src="../assets/images/logo-large.svg" alt="Typing Speed Logo" />
+        </header>
+        </>
+    );
+}
+
+export default MainTyping;
